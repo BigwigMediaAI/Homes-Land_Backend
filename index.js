39d3524f271430a48cc01bb2a4f7fdf8ccf81d09
unknown <chandanpms@gmail.com>
2025-09-08 12:12:16 +0530

    updated

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { connect } = require("./config/db");
+const leadRoutes = require("./routes/lead.route");
+const blogRoutes = require("./routes/blog.route");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/lead", leadRoutes);
+app.use("/", blogRoutes);
+
+// Start server
+app.listen(process.env.PORT, async () => {
+  try {
+    await connect();
+  } catch (error) {
+    console.error("❌ DB connection failed:", error);
+  }
+
+  console.log(`🚀 Server is listening on port ${process.env.PORT}`);
+});
